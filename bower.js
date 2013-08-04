71731d8c0bf9e1c8018af61977720da3e067beee
Olivier Scherrer <pode.fr@gmail.com>
2013-08-04 21:09:53 +0100

    add requirejs

---
+++ bower.js
@@ -0,0 +1,13 @@
+{
+  "name": "exhibition",
+  "version": "0.0.1",
+  "main": "public/index.html",
+  "ignore": [
+    "**/.*"
+  ],
+  "dependencies": {
+    "emily-prod": "1.7.0",
+    "olives-prod": "1.5.0",
+    "http-browserify": "0.1.11"
+  }
+}
