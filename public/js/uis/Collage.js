246ac6f4c11f2211f87e665eb5df7801d7247d82
Olivier Scherrer <pode.fr@gmail.com>
2013-09-14 17:59:04 +0100

    add slideshow

--- public/js/uis/Collage.js
+++ public/js/uis/Collage.js
@@ -2,22 +2,69 @@ define(function (require) {
 
 	var OObject = require("OObject"),
 		Bind = require("Bind.plugin"),
+		Events = require("Event.plugin"),
 		flickrContent = require("../services/flickrContent"),
 		Store = require("Store"),
 		Tools = require("Tools");
 
+	var hideShow = {
+		hide: function (dom) {
+			dom.style.display = "none";
+		},
+
+		show: function () {
+			dom.style.display = "";
+		}
+	}
+
 	function CollageConstructor() {
 
-		var viewModel = new Store(),
-			bind = new Bind(viewModel);
+		var collageModel = new Store([]),
+			slideShowModel = new Store({}),
+			collage = new Bind(collageModel, hideShow),
+			slideShow = new Bind(slideShowModel, hideShow),
+			events = new Events(this);
 
 		this.plugins.addAll({
-			"bind": bind
+			"collage": collage,
+			"slideShow": slideShow,
+			"events": events
 		});
+
+		slideShowModel.watchValue("main", function (photo) {
+			var index = collageModel.proxy("indexOf", photo);
+			slideShowModel.set("previous", collageModel.get(index -1));
+			slideShowModel.set("next", collageModel.get(index +1));
+			slideShowModel.set("currentMain", index);
+		});
+
+		this.startSlideShow = function startSlideShow(ev, dom) {
+			var photo = collageModel.get(collage.getItemIndex(dom));
+			slideShowModel.set("main", photo);
+			slideShowModel.set("display", true);
+		};
+
+		this.next = function next() {
+			var newMainPhoto = collageModel.get(slideShowModel.get("currentMain") +1);
+			if (newMainPhoto) {
+				slideShowModel.set("main", newMainPhoto);
+			}
+		};
+
+		this.previous = function previous() {
+			var newMainPhoto = collageModel.get(slideShowModel.get("currentMain") -1);
+			if (newMainPhoto) {
+				slideShowModel.set("main", newMainPhoto);
+			}
+		};
+
+		this.exitSlideShow = function exitSlideShow() {
+			slideShowModel.set("display", false);
+		};
 
 		this.setPhotoset = function setPhotoset(photoset) {
 			if (!photoset) { return false; }
-			viewModel.reset(photoset.map(function (photo) {
+			collageModel.reset(photoset.map(function (photo) {
 				photo.url = flickrContent.createUrl(photo ,"z");
 				return photo;
 			}));
