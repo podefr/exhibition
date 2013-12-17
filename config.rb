c40e45fec1001430bd8d51d1ecd95e06a709baca
Olivier Scherrer <pode.fr@gmail.com>
2013-12-16 22:16:02 -0500

    clean public directory

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "styles"
+css_dir = "public/styles"
 sass_dir = "SCSS"
-images_dir = "images"
-javascripts_dir = "js"
+images_dir = "public/images"
+javascripts_dir = "public/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
